77e9f0fba4d402374616ab6807f26793fe04eb6d
udaymondal <udaysk.mondal@gmail.com>
2021-02-15 17:35:02 +0600

    in process

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Humgry_Monster-Asgnt5
+# Hungry_Monster-Asgnt5
+
+
+API:
+// for search
+https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata  
--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-fetch('https://www.themealdb.com/api/json/v1/1/list.php?i=list')
-.then(res => res.json())
-.then(data => displayAllCategory(data.meals));
+const searchMeal = ()=>{
+    const serachText = document.getElementById('searchMeal').value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${serachText}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMeal(data.meals));
+}
 
-const displayAllCategory = categories => {
-    const mealCategories = document.getElementById('mealCategories');
-    //console.log(categories);
-    categories.forEach(category => {
-        console.log(categories.strIngredient);
-        
-   })
+const displayMeal = meals =>{
+    const mealContainer = document.getElementById('mealContainer');
+    mealContainer.innerHTML = '';
+    meals.forEach(meal => {
+        const mealDiv = document.createElement('div');
+        mealDiv.className = '';
+        console.log(meal.strMeal);
+    });
 }
